1c87f9a89a258326fe0752b40b3bf3ff6e15143e
Radhanaa <127480775+Radhanaa@users.noreply.github.com>
2023-12-25 23:32:32 +0700

    Add files via upload

---
+++ Dashboard/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import pickle
+import streamlit as st
+import seaborn as sns
+import plotly.express as px
+
+px.defaults.template = 'plotly_dark'
+px.defaults.color_continuous_scale = 'reds'
+
+st.sidebar.image('Thropy.png')
